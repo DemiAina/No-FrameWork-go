ad663afc7afa5839ab3dc62a0b4686c9a4c274a0
DemiAina <100835583+DemiAina@users.noreply.github.com>
2023-07-12 15:56:22 +0100

    Added: Updated main to run server

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+    
+    "flag"
+    "log"
+    "net"
+)
 
 func main(){
-    fmt.Println("Hello world.")
+    var (
+        host = flag.String("host", "", "host http address to listen on")
+        port = flag.String("port", "8000", "port number for http listener")
+    )
+    flag.Parse()
+    addr := net.JoinHostPort(*host, *port)
+    if err := runHttp(addr); err != nil {
+        log.Fatal(err)
+    }
 }
